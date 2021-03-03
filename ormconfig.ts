b8c59448d50c17125de2ce5715f60611df95856c
from20020516 <from20020516@gmail.com>
2021-03-03 17:06:57 +0900

    Add TypeORM.

---
+++ ormconfig.ts
@@ -0,0 +1,26 @@
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const ORMConfig = {
+    type: 'mysql',
+    host: process.env.DB_HOST ?? '127.0.0.1',
+    port: Number(process.env.DB_PORT) ?? 3306,
+    username: process.env.DB_USER ?? 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE ?? 'GraphTL',
+    synchronize: true,
+    logging: true,
+    entities: [
+        "src/entity/**/*.ts"
+    ],
+    migrations: [
+        "src/migration/**/*.ts"
+    ],
+    subscribers: [
+        "src/subscriber/**/*.ts"
+    ],
+    namingStrategy: new SnakeNamingStrategy(),
+}
+
+export default ORMConfig
